b08a2c93b9992bc2026cdcb41145a4f6be09f7e0
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-12-14 21:41:34 -0300

    feito requisito 09 - class Monster

---
+++ src/Monster.ts
@@ -0,0 +1,25 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  constructor(private _lifePoints = 85, private _strength = 63) { }
+
+  get strength():number { return this._strength; }
+  get lifePoints():number { return this._lifePoints; }
+
+  receiveDamage(attackPoints:number):number {
+    const damage = attackPoints > 0 ? attackPoints : 0;
+
+    const newLifePoints = this.lifePoints - damage > 0
+      ? this.lifePoints - damage : -1;
+
+    this._lifePoints = newLifePoints;
+
+    return this._lifePoints;
+  }
+  
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+}
+
+export default Monster;

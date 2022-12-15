f23e5bc822b4c715ba80a87f497ff672cbdb7715
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-12-14 22:28:20 -0300

    feito requisito 10 - class PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,24 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+const PLAYER_IS_DEAD = -1;
+
+class PVP extends Battle {
+  constructor(private _player1:Fighter, private _player2:Fighter) { 
+    super(_player1);
+  }
+
+  fight(): number {
+    while (
+      this._player1.lifePoints !== PLAYER_IS_DEAD 
+      && this._player2.lifePoints !== PLAYER_IS_DEAD
+    ) {
+      this._player1.attack(this._player2);
+      this._player2.attack(this._player1);
+    }
+
+    return super.fight();
+  }
+}
+
+export default PVP;
